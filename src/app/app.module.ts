a6c968db35cc98693c1157751579e3631c27f813
Ajinkya Pande <ajinkya.pande@tekditechnologies.com>
2023-07-31 15:34:10 +0530

    added kannada build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HindiVersionComponent } from './hindi-version/hindi-version.component';
 import { TamilVersionComponent } from './tamil-version/tamil-version.component';
 import { AutheringLocalComponent } from './authering-local/authering-local.component';
 import { AutheringServerComponent } from './authering-server/authering-server.component';
+import { KannadaVersionComponent } from './kannada-version/kannada-version.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { AutheringServerComponent } from './authering-server/authering-server.co
     HindiVersionComponent,
     TamilVersionComponent,
     AutheringLocalComponent,
-    AutheringServerComponent
+    AutheringServerComponent,
+    KannadaVersionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/kannada-version/kannada-version.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import $ from 'jquery'
+
+@Component({
+  selector: 'app-kannada-version',
+  templateUrl: './kannada-version.component.html',
+  // styleUrls: ['./kannada-version.component.css']
+  styleUrls: ['../app.component.css']
+})
+export class KannadaVersionComponent implements OnInit {
+
+  constructor() { }
+
+  title = 'speak-with-me';
+  exploreandlearn = true;
+  learnandplay = false;
+  playgame = false;
+  videohelp = false;
+
+  ngOnInit() {
+    // $(document).ready(function() {
+    //   $('.btnNext').click(function(){
+    //     $('.active').next('.nav-link').trigger('click');
+    //     });
+    // });
+  }
+  public getFingerPrintJsId = () => {
+    const fpDetails_v2 = localStorage.getItem("did");
+    return fpDetails_v2;
+  }
+
+  showExploreandLearn() {
+    this.exploreandlearn = true;
+    this.learnandplay = false;
+    this.playgame = false;
+    this.videohelp = false;
+  }
+
+  showLearnandplay() {
+    this.exploreandlearn = false;
+    this.learnandplay = true;
+    this.playgame = false;
+    this.videohelp = false;
+  }
+
+  showPlaygame() {
+    this.exploreandlearn = false;
+    this.learnandplay = false;
+    this.playgame = true;
+    this.videohelp = false;
+  }
+
+  showVideoHelp() {
+    this.exploreandlearn = false;
+    this.learnandplay = false;
+    this.playgame = false;
+    this.videohelp = true;
+  }
+
+}
+
