8602b9a17798ef0f326b6b04f33cd6d8409230f6
Gourav More <gourav_m@tekditechnologies.com>
2023-10-13 21:12:58 +0530

    new learner ai route

---
+++ src/app/learner-ai/learner-ai.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-learner-ai',
+  templateUrl: './learner-ai.component.html',
+  styleUrls: ['./learner-ai.component.css']
+})
+export class LearnerAiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
