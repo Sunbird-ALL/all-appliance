2941fa44cbd25a125cd2952bd4d66edf908065cd
deepali chavhan <deechavhan098@gmail.com>
2024-05-27 18:43:53 +0530

    TaskId #219957 fix: Added logout button in header

--- src/app/kannada-version/kannada-version.component.ts
+++ src/app/kannada-version/kannada-version.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import $ from 'jquery'
 
 @Component({
@@ -9,7 +10,9 @@ import $ from 'jquery'
 })
 export class KannadaVersionComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router : Router
+  ) { }
 
   title = 'speak-with-me';
   exploreandlearn = false;
@@ -68,6 +71,12 @@ export class KannadaVersionComponent implements OnInit {
     this.playgame = false;
     this.videohelp = false;
     this.myLearningJourney = true;
+  }
+  logout(){
+    localStorage.removeItem("profileName");
+    localStorage.removeItem("virtualId");
+    localStorage.removeItem("sessionId");
+    this.router.navigate(['/login']);
   }
 }
 
